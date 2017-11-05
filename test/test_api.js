93e78450a6f6faa374c0e76885b61c00f1e0d602
FND <fnd@localhost.localdomain>
2017-11-05 11:32:59 +0100

    started documenting internals

--- test/test_api.js
+++ test/test_api.js
@@ -1,16 +1,38 @@
 /* global describe, it */
 import Renderer, { createElement, generateHTML, safe } from "..";
+import BufferedStream from "../src/buffered-stream";
 import assert from "assert";
 
-// XXX: crude
-describe("API", _ => {
+describe("basic API features", _ => {
 	it("should export specified API", () => {
 		let renderer = new Renderer();
-		assert(renderer.registerView.call);
-		assert(renderer.renderView.call);
+		assert(renderer.registerView.name, "registerView");
+		assert(renderer.renderView.name, "renderView");
 
-		assert(createElement.call);
-		assert(generateHTML.call);
-		assert(safe.call);
+		assert.equal(createElement.name, "createElement");
+		assert.equal(generateHTML.name, "generateHTML");
+		assert.equal(safe.name, "safe");
+	});
+
+	it("should generate HTML", done => {
+		let el = generateHTML("html", { lang: "en" },
+				generateHTML("body", { id: "top" },
+						generateHTML("h1", null, "hello world"),
+						generateHTML("p", null, "lorem ipsum"),
+						callback => { // deferred child element
+							let el = generateHTML("footer", null, "…");
+							setTimeout(_ => {
+								callback(el);
+							}, 100);
+						}));
+
+		let stream = new BufferedStream();
+		el(stream, { nonBlocking: true }, function() {
+			assert.equal(arguments.length, 0);
+			assert.equal(stream.read(), '<html lang="en"><body id="top">' +
+					"<h1>hello world</h1><p>lorem ipsum</p><footer>…</footer>" +
+					"</body></html>");
+			done();
+		});
 	});
 });
